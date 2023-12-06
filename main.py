780e0f30f11f2ec03f886456fd7bd67e35d9baec
hvosdt <hvosdt@gmail.com>
2023-12-07 00:36:31 +0300

    1

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from aiogram import executor
 import logging
 import asyncio
 from handlers import *
-from aiogram.methods import DeleteWebhook
 
 
-async def main():
-    await bot(DeleteWebhook(drop_pending_updates=True))
-    await dp.start_polling(bot)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.to_thread(executor.start_polling(dp, skip_updates=True))
